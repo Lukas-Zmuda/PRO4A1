eb3970c3df2523c241a6a4158ec092d8ef2e0efb
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-10-21 13:27:41 +0200

    uloha BMI

---
+++ MojPrvy/src/UlohaBMI.java
@@ -0,0 +1,12 @@
+
+public class UlohaBMI {
+
+	public static void main(String[] args) {
+		int vyska = 186;
+		int hmotnost = 86;
+		
+		//TODO vypocitat bmi a vypisat ho v tvare BMI = hodnota
+
+	}
+
+}
