6a79eadf046f780779095b2fc933ba55f402fa58
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-10-21 13:25:21 +0200

    operatory

---
+++ MojPrvy/src/Operatory.java
@@ -0,0 +1,33 @@
+
+public class Operatory {
+
+	public static void main(String[] args) {
+
+		//aritmeticke
+		System.out.println(10 + 5);
+		System.out.println(10 - 5);
+		System.out.println(10 * 5);
+		System.out.println(10 / 3);
+		System.out.println(10 / 3.0);
+		System.out.println((double)10 / 3);
+		System.out.println(10 % 3); //vracia zvysky po deleni
+		
+		System.out.println("----------");
+		//relacne
+		System.out.println(10 < 4);
+		System.out.println(10 > 4);
+		System.out.println(10 <= 4);
+		System.out.println(10 >= 4);
+		System.out.println(10 == 4);
+		System.out.println(10 != 4);
+		
+		System.out.println("----------");
+		//logicke
+		System.out.println(!(10 < 4));
+		System.out.println((10 < 4) && (10 > 0));
+		System.out.println((10 < 4) || (10 > 0));
+		
+
+	}
+
+}
