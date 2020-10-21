24f2f88b3bc22ed85de9bfb12461b0a52d04896b
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-10-21 13:16:46 +0200

    datove typy

--- MojPrvy/src/DatoveTypy.java
+++ MojPrvy/src/DatoveTypy.java
@@ -20,13 +20,54 @@ public class DatoveTypy {
 		System.out.println(den1);
 		System.out.println("den1 = " + den1);
 		System.out.println("5" + "10");
+		System.out.println("Lukas" + "Zmuda");
 		System.out.println(5 + 10);
-		System.out.println("10 + 5 = " + 10 + 5);
+		System.out.println("10 + 5 = " + (10 + 5));
 		
+		/*
+		 * viacriadkovy komentar
+		 * 
+		 */
 		
+		byte bb = 10;
 		
+		int bbb = bb;
 		
-
+		int x = 1;
+		byte xx = (byte)x;
+		
+		x = 123456;
+		
+		//desatinne cisla
+		float f = 12.36f;
+		System.out.println("f = " + f);
+		double d = 12.9999;
+		
+		System.out.println("d = " + d);
+		int dd = (int)d;
+		System.out.println("dd = " + dd);
+		
+		d = dd;
+		
+		//znaky
+		char zavinac = '@';
+		
+		zavinac = 64;
+		System.out.println("l.zmuda" + zavinac + "ostrovskeho.com");
+		
+		char unicode = '\u0123';
+		System.out.println(unicode);
+		
+		//log. hodnoty
+		boolean prsi = false;
+		prsi = true;
+		
+		System.out.println(prsi);
+		
+		//retazce
+		String celeMeno = "";
+		String meno = "Lukas", priezvisko = "Zmuda";
+		celeMeno = meno + " " + priezvisko;
+		System.out.println(celeMeno);
 	}
-
 }
