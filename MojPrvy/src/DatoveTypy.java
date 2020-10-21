61bede63b949b84405dce9aa8e30004b96afa9a3
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-10-21 12:52:22 +0200

    1 commit

---
+++ MojPrvy/src/DatoveTypy.java
@@ -0,0 +1,32 @@
+
+public class DatoveTypy {
+
+	public static void main(String[] args) {
+		System.out.println("Ahoj");
+		
+		System.out.println(10 * 4);
+		System.out.println("10 * 4");
+		
+		int cislo;
+		cislo = 12;
+		
+		int a = 10;
+		int b = 100, c = 12;
+		
+		// int public; toto nefunguje
+		
+		//int 1den; toto nefunguje
+		int den1 = 1;
+		System.out.println(den1);
+		System.out.println("den1 = " + den1);
+		System.out.println("5" + "10");
+		System.out.println(5 + 10);
+		System.out.println("10 + 5 = " + 10 + 5);
+		
+		
+		
+		
+
+	}
+
+}
